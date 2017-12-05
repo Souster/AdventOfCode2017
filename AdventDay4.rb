a9802ac0c87125507e38b4931b87c75b791646af
mikesouster <souster@dice.fm>
2017-12-05 20:41:35 +0000

    day 4, part 2

--- AdventDay4.rb
+++ AdventDay4.rb
@@ -18,9 +18,35 @@ def validate_passphrase(phrase)
 end
 
 $number_of_successes = 0
-validate_passphrase("aa bb cc dd ee")
-validate_passphrase("aa bb cc dd aa")
-validate_passphrase("aa bb cc dd aaa")
-validate_passphrase("aaa bb cc dd aa")
+
+validate_for_anagrams("aa bb cc dd ee")
+validate_for_anagrams("aa bb cc aa dd")
+
+puts "Number of successes: #{$number_of_successes}"
+
+#Question 2
+def validate_for_anagrams(phrase)
+  words = phrase.split(" ")
+  words_arr = []
+  valid = true
+  words.each do |word|
+    sorted_word = word.split("").sort.join()
+    if !words_arr.include? sorted_word
+      words_arr.push sorted_word
+    else
+      valid = false
+      break
+    end
+  end
+  if valid
+    $number_of_successes += 1
+  end
+  puts "Validity for #{phrase}: #{valid}"
+end
+
+$number_of_successes = 0
+
+validate_for_anagrams("abcde fghij")
+validate_for_anagrams("abcde xyz ecdab")
 
 puts "Number of successes: #{$number_of_successes}"
